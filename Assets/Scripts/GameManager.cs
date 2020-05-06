6a6988162f045e2870db14e7cecd33352c43d395
Vanessa <vanessamaikojones@gmail.com>
2020-05-06 11:06:26 -0800

    lesson 5A

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(TargetSpawner());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator TargetSpawner()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1.0f);
+            int targetIndex = Random.Range(0, targets.Count);
+            Instantiate(targets[targetIndex]);
+        }
+    }
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRB = GetComponent<Rigidbody>();
+        // Randomly select launch location
+        transform.position = new Vector3(Random.Range(-5, 5), -5);
+
+        // Randomly select the upward force
+        targetRB.AddForce(Vector3.up * Random.Range(14, 19), ForceMode.Impulse);
+
+        // Randomly select the spin force
+        targetRB.AddTorque(Random.Range(-5, 5), Random.Range(-5, 5), Random.Range(-5, 5), ForceMode.Impulse);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
